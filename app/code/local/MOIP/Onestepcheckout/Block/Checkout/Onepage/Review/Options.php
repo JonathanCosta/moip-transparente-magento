367ed5f9f342955bc5b6590ab809b6aa4e60ca55
Jonathan Martins <jonathan-m.costa@hotmail.com>
2020-08-24 21:35:52 -0300

    Retirando o checkout, deixando somente a parte de pagamento

--- app/code/local/MOIP/Onestepcheckout/Block/Checkout/Onepage/Review/Options.php
+++
@@ -1,8 +0,0 @@
-<?php
-class MOIP_Onestepcheckout_Block_Checkout_Onepage_Review_Options extends Mage_Checkout_Block_Onepage_Abstract
-{
-    public function _prepareLayout()
-    {
-        return parent::_prepareLayout();
-    }
-}
